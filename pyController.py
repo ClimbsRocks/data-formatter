0c8d70c64a35c4237fce1c1dd726a2d55e352373
ClimbsRocks <climbsbytes@gmail.com>
2015-10-11 15:57:53 -0700

    adds steps for min-max normalization. need to add in intermediate steps first though

--- pyController.py
+++ pyController.py
@@ -1,15 +1,25 @@
-import concat
 import sys
 from sendMessages import printParent
 from sendMessages import messageParent
 from sendMessages import obviousPrint
+
+import concat
+import minMax
+
 # grab arguments
 trainingFile = sys.argv[1]
 testingFile = sys.argv[2]
 test = sys.argv[3]
 
 concattedResults = concat.inputFiles(trainingFile, testingFile)
+allData = concattedResults[1]
+trainingLength = concattedResults[0]
 
 if(test):
     messageParent(concattedResults, 'concat.py')
+
+minMaxNormalizedResults = minMax.normalize(allData)
+
+if(test):
+    messageParent(minMaxNormalizedResults, 'minMax.py')
 
