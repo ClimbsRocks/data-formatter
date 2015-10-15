5789fbb6d6119af98c89da477bf9e871b38cdccc
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 23:21:09 -0700

    refactors to use a before block

--- testSuite/fileConcatting.js
+++ testSuite/fileConcatting.js
@@ -8,15 +8,9 @@ module.exports = function() {
   describe('fileConcatting', function() {
     // this operation can be slow, so give it some time to process:
 
-    // if we write the results to disk, mae sure to erase them so we are starting from fresh results
-    // before(function() {
-    //   fs.unlinkSync(path.join(testFolder,'concattedtrainKaggleGiveMeSomeCredit.csv'));
-    // });
+    var results;
 
-    // this it statement describes the test. this is maybe analogous to the prompt of the question if thinking back to a university exam. 
-    // by defining done as a parameter for the callback function here, we are saying this test be an asynchronous function. 
-    // mocha will allow this test to run up until we invoke done(), or until the test times out.
-    it('should concatenate the test data and the training data together into one large data set', function(done) {
+    before(function(done) {
       var pyController = startPyTest();
 
       pyController.on('message', function(message) {
@@ -24,58 +18,36 @@ module.exports = function() {
         // we are expecting to get back an array of the concatted results
         if(message.type === 'concat.py') {
           killChildProcess(pyController.childProcess);
-          // 150,000 data rows in training data
-          // 101,503 data rows in testing data
-          expect(message.text[3].length).to.equal(251503);
+          results = message.text;
           done();
         }
-      
       });
+
+    });
+
+    // this it statement describes the test. this is maybe analogous to the prompt of the question if thinking back to a university exam. 
+    // by defining done as a parameter for the callback function here, we are saying this test be an asynchronous function. 
+    // mocha will allow this test to run up until we invoke done(), or until the test times out.
+    it('should concatenate the test data and the training data together into one large data set', function(done) {
+      // 150,000 data rows in training data
+      // 101,503 data rows in testing data
+      expect(results[3].length).to.equal(251503);
+      done();
     });
 
     it('should communicate back the number of rows in the training dataset, excluding the header row', function(done) {
-      var pyController = startPyTest();
-
-      pyController.on('message', function(message) {
-        // the first item in the array returned to us should be the length of the training data
-        if(message.type === 'concat.py') {
-          var killChildProcess = require('./killChildProcess');
-          killChildProcess(pyController.childProcess);
-          expect(message.text[2]).to.equal(150000);
-          done();
-        }
-      
-      });
+        expect(results[2]).to.equal(150000);
+        done();
     });
 
     it('should save the data description row separately', function(done) {
-      var pyController = startPyTest();
-
-      pyController.on('message', function(message) {
-        // the first item in the array returned to us should be the length of the training data
-        if(message.type === 'concat.py') {
-          killChildProcess(pyController.childProcess);
-          // console.log(message.text[0]);
-          // console.log(['id','output','continuous','continuous','categorical','continuous','continuous','categorical','categorical','categorical','categorical','categorical']);
-          expect(message.text[0]).to.deep.equal(["continuous","continuous","categorical","continuous","continuous","categorical","categorical","categorical","categorical","categorical"]);
-          done();
-        }
-      
-      });
+      expect(results[0]).to.deep.equal(["continuous","continuous","categorical","continuous","continuous","categorical","categorical","categorical","categorical","categorical"]);
+      done();
     });
 
     it('should save the header row separately', function(done) {
-      var pyController = startPyTest();
-
-      pyController.on('message', function(message) {
-        // the first item in the array returned to us should be the length of the training data
-        if(message.type === 'concat.py') {
-          killChildProcess(pyController.childProcess);
-          expect(message.text[1]).to.deep.equal(["revolvingutilizationofunsecuredlines", "age", "numberoftime30-59dayspastduenotworse", "debtratio", "monthlyincome", "numberofopencreditlinesandloans", "numberoftimes90dayslate", "numberrealestateloansorlines", "numberoftime60-89dayspastduenotworse", "numberofdependents"]);
-          done();
-        }
-      
-      });
+      expect(results[1]).to.deep.equal(["revolvingutilizationofunsecuredlines", "age", "numberoftime30-59dayspastduenotworse", "debtratio", "monthlyincome", "numberofopencreditlinesandloans", "numberoftimes90dayslate", "numberrealestateloansorlines", "numberoftime60-89dayspastduenotworse", "numberofdependents"]);
+      done();
     });
 
 
