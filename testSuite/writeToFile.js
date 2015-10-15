c85dc38b9c5281efe5585f5f80b93a8ea5970bf3
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 23:05:04 -0700

    ideas on how to test writing to a file

---
+++ testSuite/writeToFile.js
@@ -0,0 +1,13 @@
+
+
+// it should write to a file in the directory where it was invoked from by default
+// it should write to a user-specified path if passed in
+// it should write to separate testing and training files (and appropriately name them)
+// the files should be named very spcific things (ppC will rely on this)
+// each of the relevant files should be the right length
+// it should include a header row if possible (depending how we binarize, i'm not sure if this will be possible or not)
+// it should probably format the data in a nice, generalized way (utf8, etc.)
+
+
+
+
