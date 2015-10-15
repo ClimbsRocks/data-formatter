a30647e52922b2979614ea007ef2951854164ae3
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 21:17:01 -0700

    adds "csv dialect" detection, and comments

--- concat.py
+++ concat.py
@@ -5,14 +5,21 @@ from sendMessages import obviousPrint
 
 
 def inputFiles(trainingFile, testingFile):
+    # we will break out separately the ID column, the output column, and then the rest of the data
     outputData = []
     idColumn = []
     outputColumn = []
+
     with open(trainingFile, 'rU') as trainingInput:
-        trainingRows = csv.reader(trainingInput)
+        # detect the "dialect" of this type of csv file
+        dialect = csv.Sniffer().sniff(trainingInput.read(1024))
+        trainingInput.seek(0)
+        trainingRows = csv.reader(trainingInput, dialect)
+
         firstRow = 0
         for row in trainingRows:
             if firstRow < 2:
+                # grab the dataDescription row and the header row, and make them both lowercase
                 if firstRow == 0:
                     dataDescription = [x.lower() for x in row]
                 else: 
@@ -22,8 +29,6 @@ def inputFiles(trainingFile, testingFile):
                 trimmedRow = []
                 for idx, val in enumerate(row):
                     if dataDescription[idx] == 'id':
-                        # printParent('had an id event!')
-                        # printParent(val)
                         idColumn.append(val)
                     elif dataDescription[idx] == 'output':
                         outputColumn.append(val)
@@ -34,22 +39,28 @@ def inputFiles(trainingFile, testingFile):
         trainingLength = len(outputData)
 
     with open(testingFile, 'rU') as testingInput:
-        testingRows = csv.reader(testingInput)
+        # detect the dialect of the csv file
+        dialect = csv.Sniffer().sniff(testingInput.read(1024))
+        testingInput.seek(0)
+
+        testingRows = csv.reader(testingInput, dialect)
         firstRow = True
         for row in testingRows:
             if firstRow:
+                # skip the first row
                 firstRow = False
             else:
                 trimmedRow = []
                 for idx, val in enumerate(row):
                     if dataDescription[idx] == 'id':
-                        # printParent('had an id event!')
-                        # printParent(val)
                         idColumn.append(val)
                     elif dataDescription[idx] == 'output':
                         outputColumn.append(val)
                     else:
                         trimmedRow.append(val)
-
+                # NOTE: we are appending both the training and the testing data into one dataset
+                # this ensures we will be processing them consistently
+                    # if we treated them separately, it could cause an issue if we have a feature present in the testing data but not the training data, for example
                 outputData.append(trimmedRow)
+
     return [dataDescription, headerRow, trainingLength, outputData, idColumn, outputColumn]
