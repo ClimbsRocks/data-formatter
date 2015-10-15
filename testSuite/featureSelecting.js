0f237f9a5df670e5f34ea32d630805d54b668243
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 23:21:43 -0700

    refactors to use a before block

--- testSuite/featureSelecting.js
+++ testSuite/featureSelecting.js
@@ -8,12 +8,11 @@ module.exports = function() {
     // 100 minutes
     this.timeout(6000000);
 
-    // it should apply to both our training and testing data separately
-      // expect all columns to have the same length
-    it('should be applied to both our training and test data sets', function(done) {
+    var allData = [];
+
+    before(function(done) {
       var pyController = startPyTest();
 
-      var allData = [];
       pyController.on('message', function(message) {
 
         if(message.type === 'featureSelecting.py') {
@@ -21,48 +20,40 @@ module.exports = function() {
             allData.push(message.text[i]);
           }
           if (allData.length > 250000) {
-            var expectedLength = allData[0].length;
-            console.log('the length of each row after feature selection is:',expectedLength);
-            
-            function checkAllRows(matrix) {
-              for (var i = 0; i < matrix.length; i++) {
-                if ( matrix[i].length !== expectedLength ) {
-                  return false;
-                }
-              }
-              return true;
-            }
-
-            expect(checkAllRows(allData)).to.be.true;
+            killChildProcess(pyController);
             done();
           }
         }
-      
       });
+    });
+
+    // it should apply to both our training and testing data separately
+      // expect all columns to have the same length
+    it('should be applied to both our training and test data sets', function(done) {
+      var expectedLength = allData[0].length;
+      console.log('the length of each row after feature selection is:',expectedLength);
+      
+      function checkAllRows(matrix) {
+        for (var i = 0; i < matrix.length; i++) {
+          if ( matrix[i].length !== expectedLength ) {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      expect(checkAllRows(allData)).to.be.true;
+      done();
+      
     });
 
 
     // it should have fewer columns than before
     it('should should have fewer columns than before feature selection, but the same number of observations', function(done) {
-      var pyController = startPyTest();
-
-      var allData = [];
-      pyController.on('message', function(message) {
-
-        if(message.type === 'featureSelecting.py') {
-          for (var i = 0; i < message.text.length; i++) {
-            allData.push(message.text[i]);
-          }
-
-          if (allData.length > 250000) {
-            console.log('length of row after first feature selection:',allData[0].length);
-            expect(allData.length).to.equal(251503);
-            expect(allData[0]).to.have.length.below(158);
-            done();
-          }
-        }
-      
-      });
+      console.log('length of row after first feature selection:',allData[0].length);
+      expect(allData.length).to.equal(251503);
+      expect(allData[0]).to.have.length.below(158);
+      done();
     });
 
 
