9a68f3cfe40b56dc38cb8b72acd6daba847bd47e
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 23:36:13 -0700

    refactors out python contoller into before hook

--- testSuite/imputingMissingValues.js
+++ testSuite/imputingMissingValues.js
@@ -8,133 +8,110 @@ module.exports = function() {
   // TODO: add in a check for a specific row we know to be correct. 
     // this both checks to make sure things are happening to that row as we expect, and ensures that the ID column is still properly attached, if we perform this lookup by ID, ideally near the end of the dataset. 
 
-  describe('imputing missing values', function() {
-    var emptyEquivalents = ["na","n/a","none","","undefined","missing","blank","empty", undefined, NaN]
 
-    it('should not change the length or width of the matrix', function(done) {
+  describe('imputing missing values', function() {
+
+    var results;
+
+    before(function(done) {
+      console.time('imputing missing values time');
       var pyController = startPyTest();
+
 
       pyController.on('message', function(message) {
         if(message.type === 'imputingMissingValues.py') {
           killChildProcess(pyController.childProcess);
-
-          expect(message.text[0].length).to.equal(251503)
-          expect(message.text[0][0].length).to.equal(10)
+          results = message.text;
+          console.timeEnd('imputing missing values time');
           done();
         }
-      
       });
     });
 
+    var emptyEquivalents = ["na","n/a","none","","undefined","missing","blank","empty", undefined, NaN];
+
+    it('should not change the length or width of the matrix', function(done) {
+
+      expect(results[0].length).to.equal(251503)
+      expect(results[0][0].length).to.equal(10)
+      done();
+    });
+
     it('should insert a value for all completely blank values', function(done) {
-      var pyController = startPyTest();
-
-      pyController.on('message', function(message) {
-        if(message.type === 'imputingMissingValues.py') {
-          killChildProcess(pyController.childProcess);
-
-          // check every single value in the returned array to make sure it does not include any of the values listed above
-          function checkAllCorrectRanges (arr) {
-            // iterates through array
-            for (var i = 0; i < arr.length; i++) {
-              // iterates through each row. does not check ID or Output columns
-              for (var j = 0; j < arr[i].length; j++) {
-                if( emptyEquivalents.indexOf( arr[i][j] ) !== -1 ) {
-                  console.log(arr[i]);
-                  return false;
-                }
-              }
+      // check every single value in the returned array to make sure it does not include any of the values listed above
+      function checkAllCorrectRanges (arr) {
+        // iterates through array
+        for (var i = 0; i < arr.length; i++) {
+          // iterates through each row. does not check ID or Output columns
+          for (var j = 0; j < arr[i].length; j++) {
+            if( emptyEquivalents.indexOf( arr[i][j] ) !== -1 ) {
+              console.log(arr[i]);
+              return false;
             }
-            return true;
           }
-
-          expect(checkAllCorrectRanges(message.text[0])).to.be.true;
-          done();
         }
-      
-      });
+        return true;
+      }
+
+      expect(checkAllCorrectRanges(results[0])).to.be.true;
+      done();
     });
 
 
 
     it('should insert the median value of the column for missing continuous values', function(done) {
-      var pyController = startPyTest();
-
-      pyController.on('message', function(message) {
-        if(message.type === 'imputingMissingValues.py') {
-          killChildProcess(pyController.childProcess);
-          var sumOfMonthlyIncomeColumn = 0;
+      var sumOfMonthlyIncomeColumn = 0;
 
 
-          for (var i = 0; i < message.text[0].length; i++) {
-            sumOfMonthlyIncomeColumn += parseFloat( message.text[0][i][4], 10) ;
-          }
+      for (var i = 0; i < results[0].length; i++) {
+        sumOfMonthlyIncomeColumn += parseFloat( results[0][i][4], 10) ;
+      }
 
-          // previously computed value
-          // right now the answer matches exactly, but i want to leave some wiggle room if we adjust our methodology at all. 
-          expect(sumOfMonthlyIncomeColumn).to.be.within(1629324335 * .98, 1629324335 * 1.02);
-          done();
-        }
-      
-      });
+      // previously computed value
+      // right now the answer matches exactly, but i want to leave some wiggle room if we adjust our methodology at all. 
+      expect(sumOfMonthlyIncomeColumn).to.be.within(1629324335 * .98, 1629324335 * 1.02);
+      done();
     });
 
 
 
     it('should insert the most-commonly-appearing value of the column for missing categorical values', function(done) {
-      var pyController = startPyTest();
-
-      pyController.on('message', function(message) {
-        if(message.type === 'imputingMissingValues.py') {
-          killChildProcess(pyController.childProcess);
-          var countNumDependentsIsZero = 0;
+      var countNumDependentsIsZero = 0;
 
 
-          for (var i = 0; i < message.text[0].length; i++) {
-            if(message.text[0][i][9] === '0') {
-              countNumDependentsIsZero++;
-            }
-          }
-
-          // previously computed value
-          // right now the answer matches exactly, but i want to leave some wiggle room if we adjust our methodology at all. 
-          expect(countNumDependentsIsZero).to.equal(152070)
-          done();
+      for (var i = 0; i < results[0].length; i++) {
+        if(results[0][i][9] === '0') {
+          countNumDependentsIsZero++;
         }
-      
-      });
+      }
+
+      // previously computed value
+      // right now the answer matches exactly, but i want to leave some wiggle room if we adjust our methodology at all. 
+      expect(countNumDependentsIsZero).to.equal(152070)
+      done();
     });
 
 
     it('should not modify the ID or Output columns at all', function(done) {
-      var pyController = startPyTest();
+      var sumOfIdColumn = results[1].reduce(function(acc, current) {
+        return acc + parseFloat( current, 10);
+      }, 0);
 
-      pyController.on('message', function(message) {
-        if(message.type === 'imputingMissingValues.py') {
-          killChildProcess(pyController.childProcess);
+      var sumOfOutputColumn = results[2].slice(0,150000).reduce(function(acc, current) {
+        return acc + parseFloat( current, 10) ;
+      }, 0);
 
-          var sumOfIdColumn = message.text[1].reduce(function(acc, current) {
-            return acc + parseFloat( current, 10);
-          }, 0);
-
-          var sumOfOutputColumn = message.text[2].slice(0,150000).reduce(function(acc, current) {
-            return acc + parseFloat( current, 10) ;
-          }, 0);
-
-          var allOutputValuesBlank= message.text[2].slice(150000).reduce(function(acc, current) {
-            return acc && current === "";
-          }, true);
+      var allOutputValuesBlank= results[2].slice(150000).reduce(function(acc, current) {
+        return acc && current === "";
+      }, true);
 
 
-          expect(allOutputValuesBlank).to.be.true;
+      expect(allOutputValuesBlank).to.be.true;
 
-          // previously computed values
-          expect(sumOfIdColumn).to.equal(16401555256);
-          expect(sumOfOutputColumn).to.equal(10026);
-          done();
-        }
-      
-      });
+      // previously computed values
+      expect(sumOfIdColumn).to.equal(16401555256);
+      expect(sumOfOutputColumn).to.equal(10026);
+      done();
     });
 
 
