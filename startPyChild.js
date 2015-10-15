75662739e2a0bd0be76f7f1201f82f08958492db
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 23:00:04 -0700

    commands to start a python child. can likely be generalized with the test folder version

---
+++ startPyChild.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var dfDirectory = path.dirname(__filename);
+var child_process = require('child_process');
+var PythonShell = require('python-shell');
+
+
+var makePyOptions = function() {
+  var pyOptions = {
+    mode: 'json',
+    scriptPath: path.resolve(dfDirectory),
+    args: []
+  };
+  for (var i = 0; i < arguments.length; i++) {
+    pyOptions.args.push(arguments[i]);
+  }
+  return pyOptions;
+};
+
+var attachListeners = function(pyShell) {
+  pyShell.on('message', function(message) {
+    if(message.type === 'console.log') {
+      console.log('message from Python:',message.text);
+    }
+  });
+}
+
+module.exports = function(trainingDataPath, testingDataPath) {
+
+  var pyOptions = makePyOptions( trainingDataPath, testingDataPath );
+
+  var pyController = PythonShell.run('pyController.py', pyOptions, function(err) {
+    if(err) {
+      // exit code null means we killed the python child process intentionally
+      if(err.exitCode !== null) {
+        console.log('heard an error!');
+        console.error(err);
+      }
+    }
+  });
+
+  attachListeners(pyController);
+  return pyController;
+}
