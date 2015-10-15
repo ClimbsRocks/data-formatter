9abaf08db3a6744bf54c19cde3da158c2398db16
ClimbsRocks <climbsbytes@gmail.com>
2015-10-14 23:36:42 -0700

    refactors python controller into before hook

--- testSuite/dictVectorizing.js
+++ testSuite/dictVectorizing.js
@@ -7,12 +7,12 @@ module.exports = function() {
   describe('dictVectorizing', function() {
     this.timeout(600000);
 
-    it('should be performed on the combined training and testing dataset at the same time to ensure consistency', function(done) {
-      // individual row length
-      // count of columns with only 0's and 1's
+    var allData = [];
+
+    before(function(done) {
+      console.time('dict vectorizing time');
       var pyController = startPyTest();
 
-      var allData = [];
       pyController.on('message', function(message) {
 
         if(message.type === 'dictVectorizing.py') {
@@ -21,151 +21,87 @@ module.exports = function() {
           }
 
           if (allData.length > 250000) {
-            expect(allData.length).to.equal(251503);
+            killChildProcess(pyController);
+            console.timeEnd('dict vectorizing time');
             done();
           }
         }
       
       });
     });
+    
 
-    // Complete.
-    // actually, at this point, do we want to consider taking out ID and Output? 
-    // we will need them to be their own vectors for the RFECV anyways
-    // and it would let us clean up the code inside imputing a bit.
-    // we could still have the user send it over inside other properties on message, such as ID and Output, just to continue to check they're not being modified. 
-    // it('should not modify the ID or Output columns at all', function(done) {
-    //   var pyController = startPyTest();
-
-    //   var allData = [];
-    //   var
-    //   pyController.on('message', function(message) {
-    //     if(message.type === 'dictVectorizing.py') {
-    //       allData.push(message.text);
-
-    //       if ( allData.length > 250000 ) {
-    //         killChildProcess(pyController.childProcess);
-              
-    //        var sumOfIdColumn = message.text[1].reduce(function(acc, current) {
-    //          return acc + parseFloat( current, 10);
-    //        }, 0);
-
-    //        var sumOfOutputColumn = message.text[2].slice(0,150000).reduce(function(acc, current) {
-    //          return acc + parseFloat( current, 10) ;
-    //        }, 0);
-
-    //        var allOutputValuesBlank= message.text[2].slice(150000).reduce(function(acc, current) {
-    //          return acc && current === "";
-    //        }, true);
-
-
-    //        expect(allOutputValuesBlank).to.be.true;
-
-    //        // previously computed values
-    //        expect(sumOfIdColumn).to.equal(16401555256);
-    //        expect(sumOfOutputColumn).to.equal(10026);
-    //        done();
-
-    //       }
-    //     }
-      
-    //   });
-    // });
+    it('should be performed on the combined training and testing dataset at the same time to ensure consistency', function(done) {
+      // individual row length
+      // count of columns with only 0's and 1's
+      expect(allData.length).to.equal(251503);
+      done();
+    });
 
 
     it('should binarize all categorical values using one-hot encoding', function(done) {
       // individual row length
       // count of columns with only 0's and 1's
-      var pyController = startPyTest();
 
-      var allData = [];
-      pyController.on('message', function(message) {
-        if(message.type === 'dictVectorizing.py') {
-          for (var i = 0; i < message.text.length; i++) {
-            allData.push(message.text[i]);
+      var binarySummary = {};
+      for (var i = 0; i < allData[0].length; i++) {
+        binarySummary[i] = true;
+      }
+      function checkForBinaries(arr) {
+        for (var i = 0; i < arr.length; i++) {
+          for (var j = 0; j < arr[i].length; j++) {
+            if(arr[i][j] !== 0 && arr[i][j] !== 1) {
+              binarySummary[j] = false;
+            }
           }
-          if (allData.length > 250000) {
-            var binarySummary = {};
-            for (var i = 0; i < allData[0].length; i++) {
-              binarySummary[i] = true;
-            }
-            function checkForBinaries(arr) {
-              for (var i = 0; i < arr.length; i++) {
-                for (var j = 0; j < arr[i].length; j++) {
-                  if(arr[i][j] !== 0 && arr[i][j] !== 1) {
-                    binarySummary[j] = false;
-                  }
-                }
-              }
-            }
-
-            checkForBinaries(allData);
-
-            var countOfBinaryColumns = 0;
-            for (var colIndex in binarySummary) {
-              if(binarySummary[colIndex] === true) {
-                countOfBinaryColumns++;
-              }
-            }
-            // these are known sums of the combined dataset's continuous columns
-            // expect(message.text[0].length).to.equal(158);
-            expect(countOfBinaryColumns).to.equal(154);
-            done();
-          }
-
         }
-      
-      });
+      }
+
+      checkForBinaries(allData);
+
+      var countOfBinaryColumns = 0;
+      for (var colIndex in binarySummary) {
+        if(binarySummary[colIndex] === true) {
+          countOfBinaryColumns++;
+        }
+      }
+      // these are known sums of the combined dataset's continuous columns
+      // expect(message.text[0].length).to.equal(158);
+      expect(countOfBinaryColumns).to.equal(154);
+      done();
     });
 
-    // // Complete
+
     it('should include continuous columns unmodified', function(done) {
-      var pyController = startPyTest();
 
-      var allData = [];
-      pyController.on('message', function(message) {
+      // set up sumOfColumns so we don't deal with undefined values later on
+      var sumOfColumns = {};
+      for (var i = 0; i < allData[0].length; i++) {
+        sumOfColumns[i] = 0;
+      }
 
-        if(message.type === 'dictVectorizing.py') {
-          for (var i = 0; i < message.text.length; i++) {
-            allData.push(message.text[i]);
-          }
-          // allData.concat(JSON.parse(message.text));
-
-          if ( allData.length > 250000 ) {
-            killChildProcess(pyController.childProcess);
-
-            // set up sumOfColumns so we don't deal with undefined values later on
-            var sumOfColumns = {};
-            for (var i = 0; i < allData[0].length; i++) {
-              sumOfColumns[i] = 0;
-            }
-
-            function sumAllColumns (arr) {
-              for (var i = 0; i < arr.length; i++) {
-                for (var j = 0; j < arr[i].length; j++) {
-                  sumOfColumns[j] += arr[i][j];
-                }
-              }
-            }
-
-            sumAllColumns(allData);
-
-            var sums = [];
-            for (var columnIndex in sumOfColumns) {
-              // rounding to avoid differences in handling floating point numbers
-              sums.push( Math.round(sumOfColumns[ columnIndex ]) );
-            }
-
-            // these are known sums of the combined dataset's continuous columns
-            expect(sums).to.contain( 13163590 );
-            expect(sums).to.contain( 1629324335 );
-            expect(sums).to.contain( Math.round(1446246.668) );
-            expect(sums).to.contain( Math.round(87916009.34) );
-            done();
+      function sumAllColumns (arr) {
+        for (var i = 0; i < arr.length; i++) {
+          for (var j = 0; j < arr[i].length; j++) {
+            sumOfColumns[j] += arr[i][j];
           }
         }
-      
-      });
+      }
+
+      sumAllColumns(allData);
+
+      var sums = [];
+      for (var columnIndex in sumOfColumns) {
+        // rounding to avoid differences in handling floating point numbers
+        sums.push( Math.round(sumOfColumns[ columnIndex ]) );
+      }
+
+      // these are known sums of the combined dataset's continuous columns
+      expect(sums).to.contain( 13163590 );
+      expect(sums).to.contain( 1629324335 );
+      expect(sums).to.contain( Math.round(1446246.668) );
+      expect(sums).to.contain( Math.round(87916009.34) );
+      done();
     });
 
 
