d95c530b91ca88f06ea5191d5555529e89ba112e
ClimbsRocks <climbsbytes@gmail.com>
2015-10-16 08:27:41 -0700

    adds more specificity to ideas for writing to file tests

--- testSuite/writeToFile.js
+++ testSuite/writeToFile.js
@@ -4,6 +4,8 @@
 // it should write to a user-specified path if passed in
 // it should write to separate testing and training files (and appropriately name them)
 // the files should be named very spcific things (ppC will rely on this)
+  // it should include the flag that was passed in for the type of data formatting we are doing ('scikit-learn', 'brainjs','neuralNetwork')
+  // it should include the input data file name
 // each of the relevant files should be the right length
 // it should include a header row if possible (depending how we binarize, i'm not sure if this will be possible or not)
 // it should probably format the data in a nice, generalized way (utf8, etc.)
