83609cd5bb2755e7db9cf4adbe3609a783d3df95
ClimbsRocks <climbsbytes@gmail.com>
2015-10-16 08:12:45 -0700

    adds idea on testing categorical data as continuous data

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ var cwd = process.cwd();
 var startPyChild = require('./startPyChild.js');
 
 module.exports= function(args) {
+  // TODO: allow the user to pass in a flag for "decideCategoricalOrContinuous"
+    // do the data formatting process twice:
+      // once with the inputs as the user has them laid out
+      // and once where we force anything that can be a float into a float, regardless of whether the user classified it as a categorical variable or not. 
+    // then go through, train a small ensemble, and see which data set does better
 
 }
