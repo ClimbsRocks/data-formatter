39bf8f86c0e737fe9bf105d07b2a70f2a2213e8b
ClimbsRocks <climbsbytes@gmail.com>
2015-10-16 08:11:55 -0700

    adds test to make sure known useful columns are included

--- testSuite/featureSelecting.js
+++ testSuite/featureSelecting.js
@@ -50,18 +50,42 @@ module.exports = function() {
 
     // it should have fewer columns than before
     it('should should have fewer columns than before feature selection, but the same number of observations', function(done) {
-      console.log('length of row after first feature selection:',allData[0].length);
       expect(allData.length).to.equal(251503);
-      expect(allData[0]).to.have.length.below(158);
+      expect(allData[0]).to.have.length.below(100);
       done();
     });
 
+    it('should include columns that we know to be important', function(done) {
 
-    // it should keep some feature that we know to be important
-    // it should eliminate some feature that we know to be unimportant
-      // we could even consider adding in some noisy data to ensure it gets deleted
-        // but then we'd have to possibly modify previous tests
+      // set up sumOfColumns so we don't deal with undefined values later on
+      var sumOfColumns = {};
+      for (var i = 0; i < allData[0].length; i++) {
+        sumOfColumns[i] = 0;
+      }
 
+      function sumAllColumns (arr) {
+        for (var i = 0; i < arr.length; i++) {
+          for (var j = 0; j < arr[i].length; j++) {
+            sumOfColumns[j] += arr[i][j];
+          }
+        }
+      }
+
+      sumAllColumns(allData);
+
+      var sums = [];
+      for (var columnIndex in sumOfColumns) {
+        // rounding to avoid differences in handling floating point numbers
+        sums.push( Math.round(sumOfColumns[ columnIndex ]) );
+      }
+
+      // these are known sums of the combined dataset's continuous columns
+      expect(sums).to.contain( 13163590 );
+      expect(sums).to.contain( 1629324335 );
+      expect(sums).to.contain( Math.round(1446246.668) );
+      expect(sums).to.contain( Math.round(87916009.34) );
+      done();
+    });
 
   });
 };
