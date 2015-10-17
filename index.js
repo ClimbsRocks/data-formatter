fa246167e1ecf43a2a43cb29aa74f7d5ef3f0ae4
ClimbsRocks <climbsbytes@gmail.com>
2015-10-17 09:11:41 -0700

    creates output directory if it does not exist

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cwd = process.cwd();
+var path = require('path');
 var startPyChild = require('./startPyChild.js');
 
 module.exports= function(args) {
@@ -9,8 +10,34 @@ module.exports= function(args) {
       // and once where we force anything that can be a float into a float, regardless of whether the user classified it as a categorical variable or not. 
     // then go through, train a small ensemble, and see which data set does better
 
+  if( args.test === undefined ) {
+    args.test = false;
+  }
+
+  var invokingFolder = path.dirname( module.parent.filename );
+  if( args.outputFolder === undefined ) {
+    // TODO: make the output folder
+    args.outputFolder = path.join( invokingFolder, 'data-formatterResults' );
+    fs.stat( args.outputFolder, function(err, stats) {
+      if(err) {
+        console.error(err);
+      }
+      if( !stats.isDirectory() ) {
+        if( !stats.isFile() ) {
+          // the several millisecond hit to performance is not going to be noticeable compared to the minutes or hours this module will take to run, and makes the code much easier to read. 
+          fs.mkdirSync(args.outputFolder);
+        } else {
+          fs.mkdirSync( args.outputFolder + 'Folder' );
+        }
+      }
+      startPyChild(args);
+    });
+  }
+  
   // TODO:
     // figure out where this file is being invoked from
+      // module.parent.filename: https://nodejs.org/api/modules.html#modules_module_parent
+      // http://stackoverflow.com/questions/20292278/node-get-path-of-the-requiring-parent-file
     // take in flag for neural network, and handle differently than our default assumption of scikit learn and brainjs
     // take in trainingData path
     // take in testingData path
