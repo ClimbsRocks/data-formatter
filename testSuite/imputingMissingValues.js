98259db769a2c9178064005a809a5ad68fb46a89
ClimbsRocks <climbsbytes@gmail.com>
2015-10-21 22:08:06 -0700

    refactors tests for adding new columns for missing values

--- testSuite/imputingMissingValues.js
+++ testSuite/imputingMissingValues.js
@@ -12,6 +12,22 @@ module.exports = function() {
   describe('imputing missing values', function() {
 
     var results;
+    var columnSums = [];
+
+    function sumColumns(matrix) {
+      var sumObj = {};
+      for (var i = 0; i < matrix[0].length; i++) {
+        sumObj[i] = 0;
+      }
+      for( var j = 0; j < matrix.length; j++) {
+        for( var k = 0; k < matrix[i].length; k++) {
+          sumObj[k] += matrix[j][k];
+        }
+      }
+      for( var colIndex in sumObj ) {
+        columnSums.push( sumObj[colIndex] );
+      }
+    };
 
     before(function(done) {
       console.time('imputing missing values time');
@@ -22,10 +38,43 @@ module.exports = function() {
         if(message.type === 'imputingMissingValues.py') {
           killChildProcess(pyController.childProcess);
           results = message.text;
+          sumColumns( results[0] );
           console.timeEnd('imputing missing values time');
           done();
         }
       });
+    });
+
+
+    // should keep track of the total number of missing data points for each row
+    it('should keep track of the total number of missing data points for each row', function() {
+      expectedSum = 49834 + 6550;
+      expect( columnSums ).to.contain(expectedSum);
+      // TODO: sum up columns
+    });
+
+    // should keep track of each row that had a missing data point for that row
+    it('should create a boolean flag for rows that had missing data for a particular column', function() {
+      // TODO: sum up columns
+      var expectedSums = [49834, 6550];
+      expect( columnSums ).to.contain(expectedSum[0]);
+      expect( columnSums ).to.contain(expectedSum[1]);
+    });
+
+  
+
+  // should leave one copy of the original data untouched, with np.nan values for the missing values
+    it('should leave one copy of the original data untouched', function() {
+      var expectedSums = [1360220735, 186653];
+      expect( columnSums ).to.contain(expectedSum[0]);
+      expect( columnSums ).to.contain(expectedSum[1]);
+      // TODO: sum up columns
+    });
+
+  // should create two new columns for every column with missing values
+    it('should create two new columns for each column with missing values in it, and one summary column for total missing values', function() {
+      // previously computed correct result
+      expect( results[0][0].length ).to.equal(15);
     });
 
     var emptyEquivalents = ["na","n/a","none","","undefined","missing","blank","empty", undefined, NaN];
@@ -37,57 +86,61 @@ module.exports = function() {
       done();
     });
 
-    it('should insert a value for all completely blank values', function(done) {
-      // check every single value in the returned array to make sure it does not include any of the values listed above
-      function checkAllCorrectRanges (arr) {
-        // iterates through array
-        for (var i = 0; i < arr.length; i++) {
-          // iterates through each row. does not check ID or Output columns
-          for (var j = 0; j < arr[i].length; j++) {
-            if( emptyEquivalents.indexOf( arr[i][j] ) !== -1 ) {
-              console.log(arr[i]);
-              return false;
-            }
-          }
-        }
-        return true;
-      }
+    // // should impute missing values on the other copy
+    // it('should insert a value for all completely blank values', function(done) {
+    //   // check every single value in the returned array to make sure it does not include any of the values listed above
+    //   function checkAllCorrectRanges (arr) {
+    //     // iterates through array
+    //     for (var i = 0; i < arr.length; i++) {
+    //       // iterates through each row. does not check ID or Output columns
+    //       // TODO: don't check the untouched columns
+    //       for (var j = 0; j < arr[i].length; j++) {
+    //         if( emptyEquivalents.indexOf( arr[i][j] ) !== -1 ) {
+    //           console.log(arr[i]);
+    //           return false;
+    //         }
+    //       }
+    //     }
+    //     return true;
+    //   }
 
-      expect(checkAllCorrectRanges(results[0])).to.be.true;
-      done();
-    });
+    //   expect(checkAllCorrectRanges(results[0])).to.be.true;
+    //   done();
+    // });
 
 
 
     it('should insert the median value of the column for missing continuous values', function(done) {
-      var sumOfMonthlyIncomeColumn = 0;
+      // var sumOfMonthlyIncomeColumn = 0;
 
-
-      for (var i = 0; i < results[0].length; i++) {
-        sumOfMonthlyIncomeColumn += parseFloat( results[0][i][4], 10) ;
-      }
+      // // TODO: calculate sum of all columns
+      //   // compare against expected results with imputing
+      // for (var i = 0; i < results[0].length; i++) {
+      //   sumOfMonthlyIncomeColumn += parseFloat( results[0][i][4], 10) ;
+      // }
 
       // previously computed value
       // right now the answer matches exactly, but i want to leave some wiggle room if we adjust our methodology at all. 
-      expect(sumOfMonthlyIncomeColumn).to.be.within(1629324335 * .98, 1629324335 * 1.02);
+
+      expect(columnSums).to.contain(1629324335)
+      // expect(sumOfMonthlyIncomeColumn).to.be.within(1629324335 * .98, 1629324335 * 1.02);
       done();
     });
 
 
 
     it('should insert the most-commonly-appearing value of the column for missing categorical values', function(done) {
-      var countNumDependentsIsZero = 0;
+      // var countNumDependentsIsZero = 0;
 
-
-      for (var i = 0; i < results[0].length; i++) {
-        if(results[0][i][9] === '0') {
-          countNumDependentsIsZero++;
-        }
-      }
-
-      // previously computed value
-      // right now the answer matches exactly, but i want to leave some wiggle room if we adjust our methodology at all. 
-      expect(countNumDependentsIsZero).to.equal(152070)
+      // // TODO: simply check sums again
+      // for (var i = 0; i < results[0].length; i++) {
+      //   if(results[0][i][9] === '0') {
+      //     countNumDependentsIsZero++;
+      //   }
+      // }
+      // // previously computed value
+      // expect(countNumDependentsIsZero).to.equal(152070)
+      expect(columnSums).to.contain(152070)
       done();
     });
 
