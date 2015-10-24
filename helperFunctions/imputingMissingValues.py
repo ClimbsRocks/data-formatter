1b24219a3c786c510f958a1d8e17a34d40d8d9af
ClimbsRocks <climbsbytes@gmail.com>
2015-10-24 09:24:23 -0700

    refactoring to only check known missing columns and find median manually

--- helperFunctions/imputingMissingValues.py
+++ helperFunctions/imputingMissingValues.py
@@ -101,32 +101,45 @@ def impute( columnMatrix, dataDescription, colMap ):
     # fillInVals will have keys for each column index, and values for what the filled in value should be
         # this way we only need to check continuous or categorical once
     fillInVals = {}
-    for colIndex, column in enumerate(columnMatrix):
-        # FUTURE: we can calculate this for only columns that are actually missing data
-        if dataDescription[ colIndex ] == 'continuous':
-        # TODO: calculate median myself
-            # copy the list
-            copiedList = list(column)
-            # sort the list
-            copiedList.sort()
-            # find the index of None
-            noneIndex = copiedList.index(None)
-                # might have to sort the list descending or ascending for the code to be obvious
-            # divide that number in half (make it an int)
-            medianIndex = int( noneIndex / 2 )
-            # access that position in the copied & sorted list
-            medianVal = copiedList[ medianIndex ]
-            # store that number into fillInVals
-            fillInVals[ colIndex ] = medianVal
-            # delete that sorted list
+    # for colIndex, column in enumerate(columnMatrix):
+        # do this only for columns with missing values
+    for colIndex in colMap:
+        try:
+            colIndex = int( colIndex )
+            # FUTURE: we can calculate this for only columns that are actually missing data
+            if dataDescription[ colIndex ] == 'continuous':
+            # TODO: calculate median myself
+                # copy the list
+                copiedList = list( columnMatrix[ colIndex ])
+                printParent('made it into if statement for continuous')
+                # sort the list
+                copiedList.sort()
+                # find the index of None
+                # printParent(copiedList)
+                for rowIndex, value in enumerate(copiedList):
+                    if value == None:
+                        noneIndex = rowIndex
+                # noneIndex = copiedList.index(None)
+                    # might have to sort the list descending or ascending for the code to be obvious
+                # divide that number in half (make it an int)
+                medianIndex = int( noneIndex / 2 )
+                # access that position in the copied & sorted list
+                medianVal = copiedList[ medianIndex ]
+                # store that number into fillInVals
+                fillInVals[ colIndex ] = medianVal
+                # delete that sorted list
 
-            # the np way of doing this assumes that None is a value, and includes it when calculating the median. 
-            # what we really want is the median value excluding the missing values
-            # the median value
-            # fillInVals[ colIndex ] = np.median(np.array(column))
-        elif dataDescription[ colIndex ] == 'categorical':
-            # the mode value
-            fillInVals[ colIndex ] = max(set(column), key=column.count)
+                # the np way of doing this assumes that None is a value, and includes it when calculating the median. 
+                # what we really want is the median value excluding the missing values
+                # the median value
+                # fillInVals[ colIndex ] = np.median(np.array(column))
+            elif dataDescription[ colIndex ] == 'categorical':
+                # the mode value
+                fillInVals[ colIndex ] = max(set(column), key=column.count)
+        except: 
+            pass
+            # printParent('we failed to create a fillInVals value for this key')
+            # printParent(colIndex)
 
     printParent('fillInVals')
     printParent(fillInVals)
