7a88ca8d0302ff4deb1e3225102e077984302d6b
ClimbsRocks <climbsbytes@gmail.com>
2015-10-24 22:07:53 -0700

    adds ability to invoke from the command line

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
-var fs = require('fs');
-var path = require('path');
+#!/usr/bin/env node
 
-var mkdirp = require('mkdirp');
+(function() {
 
-var startPyChild = require('./helperFunctions/startPyChild.js');
+  var fs = require('fs');
+  var path = require('path');
+  var mkdirp = require('mkdirp');
+  var startPyChild = require('./helperFunctions/startPyChild.js');
 
-var cwd = process.cwd();
+  var cwd = process.cwd();
 
-module.exports= function( args, callback ) {
-  // the following is a way-past-MVP idea that probably makes more sense for users to do themselves. 
-  // TODO: allow the user to pass in a flag for "decideCategoricalOrContinuous"
-    // do the data formatting process twice:
-      // once with the inputs as the user has them laid out
-      // and once where we force anything that can be a float into a float, regardless of whether the user classified it as a categorical variable or not. 
-    // then go through, train a small ensemble, and see which data set does better
+  module.exports= function( args, callback ) {
+    // the following is a way-past-MVP idea that probably makes more sense for users to do themselves. 
+    // TODO: allow the user to pass in a flag for "decideCategoricalOrContinuous"
+      // do the data formatting process twice:
+        // once with the inputs as the user has them laid out
+        // and once where we force anything that can be a float into a float, regardless of whether the user classified it as a categorical variable or not. 
+      // then go through, train a small ensemble, and see which data set does better
 
-  if( args.test === undefined ) {
-    args.test = false;
-  }
-
-  var invokingFolder = path.dirname( module.parent.filename );
-
-  if( args.outputFolder === undefined ) {
-    args.outputFolder = path.join( invokingFolder, 'data-formatterResults' );
-  }
-
-  // make sure the output folder exists. if not, create it.
-  mkdirp( args.outputFolder, function(err) {
-    if(err) {
-      console.error(err);
+    if( args.test === undefined ) {
+      args.test = false;
     }
-    startPyChild( args, callback );
-  });
-  
-};
+
+    if( args.outputFolder === undefined ) {
+      var invokingFolder = path.dirname( module.parent.filename );
+      args.outputFolder = path.join( invokingFolder, 'data-formatterResults' );
+    }
+
+    // make sure the output folder exists. if not, create it.
+    mkdirp( args.outputFolder, function(err) {
+      if(err) {
+        console.error(err);
+      }
+      startPyChild( args, callback );
+    });
+    
+  };
+
+  // allow the module to be invoked from the command line
+  // since this is all wrapped in an IIFE, this if statement will execute and check if data-formatter was invoked from another module, or without a parent (from the command line)
+  if( !module.parent ) {
+    var argsObj = {};
+    // if we are being invoked from the command line, output the formatted data into this same directory. 
+    argsObj.outputFolder = path.join( cwd, 'data-formatterResults');
+    argsObj.trainingData = process.argv[2];
+    argsObj.testingData = process.argv[3];
+
+
+    module.exports( argsObj );
+  }
+
+})();
