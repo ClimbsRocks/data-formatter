b647389143483e4ebd99b76d0c2daeac6a7f0da2
ClimbsRocks <climbsbytes@gmail.com>
2015-10-25 10:39:36 -0700

    adds much more logging for the user when validating input row lengths

--- helperFunctions/validation.py
+++ helperFunctions/validation.py
@@ -25,3 +25,32 @@ def dataDescription(arr):
     if( not expectedValues['output'] ):
         printParent('Warning, there is no column with an "Output" label in the first row')
         raise TypeError('dataDescription row incomplete')
+
+def rowLength( row, expectedRowLength, rowCount ):
+    if len( row ) != expectedRowLength:
+        printParent( 'This row did not have the same number of columns as the dataDescription row.')
+        printParent( row )
+        printParent( 'This is row number:')
+        printParent( rowCount )
+        printParent( 'Please make sure that all rows have the same number of columns, even if those values are blank')
+        printParent( 'And it might be worth double checking that your dataDescription row has an accurate description for each column in the dataset')
+
+def testingHeaderRow( row, expectedRowLength ):
+    if len( row ) != expectedRowLength:
+        printParent('len(row)')
+        printParent(len(row))
+        printParent('len( outputData[ 0 ]')
+        printParent(len( outputData[ 0 ] ) )
+        printParent('we noticed that the testing and training datasets have different numbers of columns')
+        printParent('we are going to assume that the "Output" column(s) is(are) simply not included for the testing dataset.')
+        return False
+    return True
+
+def testingRowLength( row, expectedRowLength, rowCount ):
+    if len( row ) != expectedRowLength:
+        printParent( 'This row did not have the same number of columns as the testing dataset header row.')
+        printParent( row )
+        printParent( 'Within the testing dataset, this is row number:')
+        printParent( rowCount )
+        printParent( 'Please make sure that all rows have the same number of columns, even if those values are blank')
+
