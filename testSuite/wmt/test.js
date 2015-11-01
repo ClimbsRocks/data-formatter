6ae0b107dae9782ea6ae0a0c22b48ee59138f0f5
ClimbsRocks <climbsbytes@gmail.com>
2015-10-31 20:37:58 -0700

    data set for IDs spread across multiple rows from kaggle's Wal-Mart Shopping Trip Classifier competition

---
+++ testSuite/wmt/test.js
@@ -0,0 +1,28 @@
+
+var expect = require('chai').expect;
+var mocha = require('mocha');
+var startPyTest = require('../startPyTest');
+
+// this block will contain all the tests for the entire data-formatter package
+describe('datasets with multiple rows for each ID', function() {
+
+  global.pyControllerWmt = startPyTest('wmt');
+
+  // // this timeout should be long enough to handle tests on a variety of machines
+  // this.timeout(360000);
+
+  // fileConcatting();
+
+  // imputingMissingValues();
+
+  // dictVectorizing();
+
+  // featureSelecting();   
+
+  // neuralNetwork(); 
+  
+  // brainjsTest();
+
+  // fileNames();
+
+});
