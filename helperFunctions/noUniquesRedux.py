6e0e21a419452d75d26858e2b8f99c6f80533843
ClimbsRocks <climbsbytes@gmail.com>
2015-11-01 22:04:29 -0800

    adds placeholder function for deleting non-unique values

---
+++ helperFunctions/noUniquesRedux.py
@@ -0,0 +1,15 @@
+from sendMessages import printParent
+from sendMessages import messageParent
+from sendMessages import obviousPrint
+
+def clean(X):
+    columnCounts = {}
+    for rowObj in X:
+        for key in rowObj:
+            try:
+                columnCounts[key] += 1
+            except:
+                columnCounts[key] = 1
+    # printParent( columnCounts )
+    # printParent( len(columnCounts.keys()) )
+    return X
