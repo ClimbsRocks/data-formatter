39e8b620398f29271f5718931bcfd3b95da8d7e0
ClimbsRocks <climbsbytes@gmail.com>
2015-11-02 22:06:22 -0800

    adds MaxAbsScaler for sparse data, a new feature in scikit-learn 0.17

--- helperFunctions/minMax.py
+++ helperFunctions/minMax.py
@@ -5,8 +5,10 @@ from sendMessages import messageParent
 from sendMessages import obviousPrint
 
 min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1), copy=False)
+max_abs_scaler = preprocessing.MaxAbsScaler()
 
 # Note: we're assuming that binarization of categorical data has already happened, and we don't need to do a check for categorical data here. 
 def normalize( X ):
-    X = min_max_scaler.fit_transform(X)
+    # X = min_max_scaler.fit_transform(X)
+    X = max_abs_scaler.fit_transform(X)
     return X
