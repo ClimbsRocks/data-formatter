ad05a240d24fc53eb6322621c1c2cf3317887d43
ClimbsRocks <climbsbytes@gmail.com>
2015-11-03 23:50:38 -0800

    adds checks for additional flexibility (different ignored columns, no id column, etc.)

--- helperFunctions/validation.py
+++ helperFunctions/validation.py
@@ -3,6 +3,7 @@ from sendMessages import messageParent
 from sendMessages import obviousPrint
 
 def dataDescription(arr):
+    expectedTestRowLength = 0
     expectedValues = {
         'id': False,
         'output': False
@@ -13,18 +14,50 @@ def dataDescription(arr):
         try:
             allowableValues.index(name)
             expectedValues[name] = True
+            # sometimes we will include columns in our training dataset that we will not include in our testing dataset. we want to allow for that
+            # we already have logic in place for handling missing output values in our testing dataset. 
+            if name not in ['ignore']:
+                expectedTestRowLength += 1
+
         except:
             printParent('Warning, we have received a value in the first row that is not valid:')
             printParent(name)
             printParent('Please remember that the first row must contain information describing that column of data')
             printParent('Acceptable values are: "ID", "Output", "Continuous", and "Categorical"')
             raise
-    if( not expectedValues['id'] ):
-        printParent('Warning, there is no column with an "ID" label in the first row')
-        raise TypeError('dataDescription row incomplete')
     if( not expectedValues['output'] ):
         printParent('Warning, there is no column with an "Output" label in the first row')
         raise TypeError('dataDescription row incomplete')
+
+    if( not expectedValues['id'] ):
+        printParent('Warning, there is no column with an "ID" label in the first row')
+        # our testing dataset must have an id in it, so if our training data does not have an id column, we would expect our testing data to have one more column
+        expectedTestRowLength += 1
+        return False, expectedTestRowLength
+        raise TypeError('dataDescription row incomplete')
+
+    # returning True means that we do have all the pieces we need to continue as normal
+    return True, expectedTestRowLength
+
+def isTestingDataDescription(row):
+    lowerRow = [x.lower() for x in row]
+    printParent('row inside isTestingDataDescription:')
+    printParent(lowerRow)
+    if 'continuous' in lowerRow or 'categorical' in lowerRow:
+        return True
+    else:
+        return False
+
+def checkColumnCounts(row, expectedRowLength):
+    # the testing data must have an ID, so we are not checking for that
+    # don't count (ignore, even!) the ignore columns
+    rowLength = len(row)
+    ignoredColumns = [x for x in row if x =='ignore']
+    rowLength -= len(ignoredColumns)
+    if rowLength == expectedRowLength:
+        return True
+    else:
+        return False
 
 def rowLength( row, expectedRowLength, rowCount ):
     if len( row ) != expectedRowLength:
