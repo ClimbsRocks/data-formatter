6e42dd4912b92c9cb26c6dc213ec5444c313759d
ClimbsRocks <climbsbytes@gmail.com>
2015-11-05 22:37:21 -0800

    creates structure for joining data tests

---
+++ testSuite/joinData/test.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+var mocha = require('mocha');
+var startPyTest = require('../startPyTest');
+var joiningData = require('./joiningData');
+
+// this block will contain all the tests for the entire data-formatter package
+describe('joining external data into our training and testing files', function() {
+
+  global.pyControllerRossman = startPyTest('rossman');
+
+  // // this timeout should be long enough to handle tests on a variety of machines
+  this.timeout(360000);
+
+  joiningData();
+
+});
