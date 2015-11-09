a28533e2c3eb6aa3d8be441d43976d24dc2163c0
ClimbsRocks <climbsbytes@gmail.com>
2015-11-08 16:18:23 -0800

    writes sparse matrices for idColumn and y values as well.

--- helperFunctions/writeToFile.py
+++ helperFunctions/writeToFile.py
@@ -3,19 +3,48 @@ import csv
 import os.path as path
 import numpy as np
 
+from scipy.sparse import csr_matrix
+
 from sendMessages import printParent
 from sendMessages import messageParent
 from sendMessages import obviousPrint
 
-# Pre: make a folder in Node.js, and pass in that file path. That is an intermediate folder. 
-# A. grab name of training data file
-# B. 
-# 1. write ID column to it's own file
-# 2. write y column(s) to it's own file
-# 3. write X training to it's own file
-# 4. write X testing to it's own file
 
-def writeMetadata(y, idColumn, args, headerRow ):
+# shoutout to the original author:
+# http://stackoverflow.com/questions/8955448/save-load-scipy-sparse-csr-matrix-in-portable-data-format
+def save_sparse_csr(filename,array):
+    np.savez(filename,data=array.data ,indices=array.indices, indptr=array.indptr, shape=array.shape )
+
+
+def writeMetadata(y, idColumn, args, headerRow):
+    y_train= path.join( args['outputFolder'], 'y_train_' + args['trainingPrettyName'] + '.csv' )
+    id_train= path.join( args['outputFolder'], 'id_train_' + args['trainingPrettyName'] + '.csv' )
+    id_test= path.join( args['outputFolder'], 'id_test_' + args['testingPrettyName'] + '.csv' )
+
+    trainingLength = args['trainingLength']
+
+    idTrainData = np.array( idColumn[ 0 : trainingLength ] )
+    idTestData = np.array( idColumn[ trainingLength : ] )
+    y = np.array( y ) 
+
+    y = [int(i) for i in y[0:trainingLength]]
+    idTrainData = [int(i) for i in idTrainData]
+    idTestData = [int(i) for i in idTestData]
+
+    save_sparse_csr(y_train, csr_matrix(y) )
+    save_sparse_csr(id_train, csr_matrix( idTrainData ) )
+    save_sparse_csr(id_test, csr_matrix( idTestData ))
+
+    
+    fileNames = {
+        'y_train': y_train,
+        'id_train': id_train,
+        'id_test': id_test
+    }
+    messageParent( fileNames, 'fileNames' )
+    
+
+def writeMetadataDense(y, idColumn, args, headerRow ):
     # grab the name of the training and testing files from the full path to those datasets
 
     # trainingFileName = path.split( args['trainingData'] )[ -1 ]
@@ -122,11 +151,6 @@ def writeDataSparse(X, args, headerRow, nn ):
     # save the file names into variables- we will use them to create the file and in the fileNames hash messaged out to the parent.
     X_train= path.join( args['outputFolder'],  'X_train_' + trainingFileName )
     X_test= path.join( args['outputFolder'], 'X_test_' + testingFileName )
-
-    # lifted directly from stackOverflow:
-    # http://stackoverflow.com/questions/8955448/save-load-scipy-sparse-csr-matrix-in-portable-data-format
-    def save_sparse_csr(filename,array):
-        np.savez(filename,data=array.data ,indices=array.indices, indptr=array.indptr, shape=array.shape )
 
     # scipy sparse matrices need a list of indices to slice
     # http://stackoverflow.com/questions/13352280/slicing-sparse-matrices-in-scipy-which-types-work-best
