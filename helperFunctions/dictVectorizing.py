daef1892c88fb5f60dbb3bbfa118b90dcee7085a
ClimbsRocks <climbsbytes@gmail.com>
2015-12-07 23:31:52 -0800

    adds command to delete listOfDicts

--- helperFunctions/dictVectorizing.py
+++ helperFunctions/dictVectorizing.py
@@ -3,6 +3,8 @@ from sendMessages import printParent
 from sendMessages import messageParent
 from sendMessages import obviousPrint
 
+from scipy.sparse import csr_matrix
+
 
 vectorizer = DictVectorizer()
 
@@ -13,9 +15,13 @@ def vectorize(listOfDicts):
         # then, feed 1 item at a time through transform
         # as we do, delete the dict representation of that item
         # this way we are only holding one copy of the data in memory at a time
+
+    # vectorizer.fit(listOfDicts)
+    # sparseMatrix = csr_matrix()
         
     sparseMatrix = vectorizer.fit_transform(listOfDicts)
     for dictIdx, rowDict in enumerate(listOfDicts):
         listOfDicts[dictIdx] = None
         del rowDict
+    del listOfDicts
     return [ sparseMatrix, vectorizer.feature_names_, vectorizer.vocabulary_ ]
